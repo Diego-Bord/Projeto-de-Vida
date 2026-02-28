ab1d10da5d3030ed6ed56b359139be0f3688be6b
Diego-Bord <d.bordignon@escola.pr.gov.br>
2026-02-28 11:28:28 -0300

    Atualização dos Contadores V3

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 
 // Configuração dos Cronômetros
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2027-12-05T00:00:00");
-const tempoObjetivo2 = new Date("2026-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2028-01-30T00:00:00");
-const tempoObjetivo4 = new Date("2030-01-01T00:00:00");
+const tempoObjetivo1 = new Date("2030-01-04T00:00:00");
+const tempoObjetivo2 = new Date("2028-01-03T00:00:00");
+const tempoObjetivo3 = new Date("2026-12-02T00:00:00");
+const tempoObjetivo4 = new Date("2027-12-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
